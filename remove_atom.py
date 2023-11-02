03c92877a81806bd020fb2127021add0bd500acc
Mikel <mikelamg27@gmail.com>
2023-11-02 13:38:31 -0600

    Fixed remove atom so it updates the coordinate to index variable

--- remove_atom.py
+++ remove_atom.py
@@ -1,4 +1,4 @@
-def remove_atom_and_update_triplets(atom_to_remove, found_triplets, atom_triplets, triplet_energies, total_energy):
+def remove_atom_and_update_triplets(atom_to_remove, found_triplets, atom_triplets, triplet_energies, total_energy, coord_to_index):
     # Remove triplets associated with the atom
     triplets_to_remove = atom_triplets[atom_to_remove]
     
@@ -13,5 +13,18 @@ def remove_atom_and_update_triplets(atom_to_remove, found_triplets, atom_triplet
     for triplet in triplets_to_remove:
         del triplet_energies[triplet]
 
-        
-    return new_total_energy, found_triplets, triplet_energies
+    # Remove the entries for the atom from atom_triplets
+    del atom_triplets[atom_to_remove]
+    
+    # Update coord_to_index mapping and indices in atom_triplets
+    for coord, index in coord_to_index.items():
+        if index > atom_to_remove:
+            coord_to_index[coord] -= 1
+
+    new_atom_triplets = {}
+    for index, triplets in atom_triplets.items():
+        new_index = index - 1 if index > atom_to_remove else index
+        new_atom_triplets[new_index] = triplets
+    
+    return new_total_energy, found_triplets, triplet_energies, new_atom_triplets, coord_to_index
+
